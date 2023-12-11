3220876851be59c7b718d1ce30f832a81373a83b
Oleksiy <oleksiy.kondratyuk.work@gmail.com>
2023-12-11 21:30:26 +0300

    commit 4

--- GitExercises/Program.cs
+++ GitExercises/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Some shanges!");
-Console.WriteLine("Some shanges2!");
+Console.WriteLine("Some shanges3!");
